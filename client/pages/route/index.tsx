06cbf0a82c8a6d4565d3e7ab4e2248080c5c0c0b
Profilist <84641209+Profilist@users.noreply.github.com>
2024-08-17 02:02:25 -0400

    Added pins of stores to map

--- client/pages/route/index.tsx
+++ client/pages/route/index.tsx
@@ -3,11 +3,37 @@ import {
   APIProvider,
   Map,
   MapCameraChangedEvent,
+  AdvancedMarker,
+  Pin,
 } from "@vis.gl/react-google-maps";
 import dotenv from "dotenv";
 import styles from "./index.module.css";
 
 dotenv.config();
+
+type Poi = { key: string; location: google.maps.LatLngLiteral };
+const locations: Poi[] = [
+  { key: "noFrills", location: { lat: 43.8547691, lng: -79.4297517 } },
+  { key: "freshCo", location: { lat: 43.88075864412101, lng: -79.39528998999123 } },
+  { key: "foodBasics", location: { lat: 43.87763667809579, lng: -79.41134453245378 } },
+  { key: "tnt", location: { lat: 43.8622441, lng: -79.4326858 } },
+];
+
+const PoiMarkers = (props: { pois: Poi[] }) => {
+  return (
+    <>
+      {props.pois.map((poi: Poi) => (
+        <AdvancedMarker key={poi.key} position={poi.location}>
+          <Pin
+            background={"#F92D2D"}
+            glyphColor={"#000"}
+            borderColor={"#000"}
+          />
+        </AdvancedMarker>
+      ))}
+    </>
+  );
+};
 
 const index = () => {
   const token = process.env.googleToken as string;
@@ -19,8 +45,9 @@ const index = () => {
     >
       <Map
         className={styles.map}
-        defaultZoom={14}
+        defaultZoom={13}
         defaultCenter={{ lat: 43.855, lng: -79.4 }}
+        mapId={"3ccec7981803cd35"}
         onCameraChanged={(ev: MapCameraChangedEvent) =>
           console.log(
             "camera changed:",
@@ -29,7 +56,9 @@ const index = () => {
             ev.detail.zoom
           )
         }
-      ></Map>
+      >
+        <PoiMarkers pois={locations} />
+      </Map>
     </APIProvider>
   );
 };
