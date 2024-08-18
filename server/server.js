6de6a1dad1fbec6f8ee1472103f33d5961a66b22
Profilist <84641209+Profilist@users.noreply.github.com>
2024-08-18 02:44:52 -0400

    Updated PORT

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = "https://the-fastest-root-server.vercel.app/";
+const PORT = 8080;
 const cors = require("cors");
 const {
   scrapNoFrills,
